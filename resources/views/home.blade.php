a6b2b5f34c53bf4ce439450b5cefe9831a24c8dd
MUHAMMAD GILANG <gilang.ariyana@widyatama.ac.id>
2023-06-13 12:17:26 +0700

    Home Page beres (belum responsif)

---
+++ resources/views/home.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Link CSS -->
+    <link rel="stylesheet" href="css/home-design.css">
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500&display=swap" rel="stylesheet">
+
+    <!-- Side Navbar Logos -->
+    <script src="https://kit.fontawesome.com/1d9af05b36.js"></script>
+
+    <title>Japan Quiz | Home</title>
+</head>
+<body>
+
+    <div class="container">
+        <!-- Side Navigation Bar -->
+        <div class="sidebar">
+            <h1>LOGO</h1>
+            <hr class="new1">
+            <ul>
+                <li><a href="/" ><i class="fa-solid fa-house"></i>Home</a></li>
+                <li><a href="/quiz" ><i class="fa-solid fa-book"></i>Quiz</a></li>
+                <li><a href="/dictionary" ><i class="fa-solid fa-language"></i>Dictionary</a></li>
+            </ul>
+        </div>
+        <!-- Main Content -->
+        <div class="main_content">
+            <div class="wrapper"></div>
+            <div class="text_button">
+                <h3>The online Japanese quiz that's easy to use and fun</h3>
+                <p>Try a quiz that engages students, generates leads</p>
+                <button>Try Quiz</button>
+            </div>
+            <h1 class="vertical" lang=ja>クイズ</h1>
+        </div>
+    </div>
+
+</body>
+</html>
