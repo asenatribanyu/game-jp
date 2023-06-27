f366b60ef34b242c02c80f74574b20a3b734fbfd
salsabilash <80380796+salsabilash@users.noreply.github.com>
2023-06-27 20:02:41 +0700

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/dictionary', function () {
+    return view('dictionary');
+});
+
+
+Route::get('/dict-hiragana', function () {
+    return view('hiragana');
+})->name('hiragana');
+
+Route::get('/dict-katakana', function () {
+    return view('katakana');
+})->name('katakana');
